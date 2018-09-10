f2968dea1fdf08fc2e7a588b64b4e4e8d4fae338
Kevin Malan <kevmalan@outlook.com>
2018-09-10 07:52:35 +0200

    adds Queue monitoring

---
+++ FileOperations.FileWatcher/FileOperations.FileImporter/Models/FileMetadata.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FileOperations.FileImporter.Models
+{
+    public class FileMetadata
+    {
+        public string FilePath { get; set; }
+        public DateTime PeriodFromDate { get; set; }
+        public DateTime PeriodToDate { get; set; }
+    }
+}
---
+++ FileOperations.FileWatcher/FileOperations.FileImporter/Services/FileImportService.cs
@@ -0,0 +1,43 @@
+﻿using FileOperations.FileImporter.Models;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Messaging;
+
+namespace FileOperations.FileImporter.Services
+{
+    public class FileImportService
+    {
+        private readonly MessageQueue _messageQueue;
+
+        public FileImportService()
+        {
+            const string queue = @".\Private$\FileQueue";
+
+            _messageQueue = MessageQueue.Exists(queue) ? MessageQueue.Create(queue) : new MessageQueue(queue);
+            _messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(FileMetadata) });
+        }
+
+        public void HandleFileImport()
+        {
+            // Reads first item in the Queue
+            var message = _messageQueue.Receive(MessageQueueTransactionType.Single);
+            FileMetadata fileMetadata = (FileMetadata)message?.Body;
+
+            if (fileMetadata != null)
+            {
+                // TODO: Implement logic to handle the fileMetadata object.
+            }
+
+            MoveProcessedFile(fileMetadata?.FilePath);
+        }
+
+        private void MoveProcessedFile(string path)
+        {
+            string fileProcessedPath = ConfigurationManager.AppSettings["filesProcessedLocation"];
+            string destinationFile = $"{fileProcessedPath}\\{Path.GetFileName(path)}";
+
+            File.Move(path, destinationFile);
+        }
+    }
+}
